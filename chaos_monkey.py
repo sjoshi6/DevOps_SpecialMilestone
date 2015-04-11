5031a433af7dcacc3719373fd489a1131263caff
Saurabhh <sjoshi6@ncsu.edu>
2015-04-11 10:53:03 -0400

    chaos monkey additions

--- chaos_monkey.py
+++ chaos_monkey.py
@@ -4,9 +4,13 @@ import sys
 def chaos_monkey():
     conn = boto.ec2.connect_to_region(sys.argv[1],aws_access_key_id=sys.argv[2], aws_secret_access_key=sys.argv[3])
     reservations = conn.get_all_reservations()
-    instances = reservations[0].instances
-    instance= instances[0]
-    print(instance.instance_type)
+    instance_list = []
+    for reservation in reservations:
+        instances = reservation.instances
 
+        for instance in instances:
+            instance_list.append(instance)
+
+    print(instance_list)
 
 chaos_monkey()
